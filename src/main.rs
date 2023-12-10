72ce349035aa32fd52fc43e6cbb4d8e0e1686b10
Aaron Brodersen <aaron@abrodersen.com>
2023-12-09 21:42:52 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,93 @@
+
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use git2::{Repository, ObjectType};
+use serde::{Serialize, Deserialize};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    repo: String,
+    #[arg(long)]
+    old_rev: String,
+    #[arg(long)]
+    new_rev: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ImageConfig {
+    image: String,
+    tag:   String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BuildSpec {
+    image: String,
+    tag:   String,
+    path:  PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::try_parse()?;
+    let repo = Repository::open(&args.repo)?;
+    let repo_path = Path::new(&args.repo);
+
+    let t1 = repo
+        .revparse_single(&args.old_rev)
+        .context("failed to find old revision")?
+        .peel(ObjectType::Tree)?;
+    let t2 = repo
+        .revparse_single(&args.new_rev)
+        .context("failed to find new revision")?
+        .peel(ObjectType::Tree)?;
+
+    let diff = repo.diff_tree_to_tree(t1.as_tree(), t2.as_tree(), None)?;
+
+    let specs = diff.deltas()
+        .map(|d| {
+            let new_path = match d.new_file().path() {
+                Some(p) => p,
+                None => return Ok(None),
+            };
+            let f_path   = repo_path.join(new_path);
+            let f_parent = f_path.parent().context("unexpected missing parent")?;
+            let d_full   = f_parent.canonicalize().context("failed to canonicalize path")?;
+            Ok(Some(d_full))
+        })
+        .collect::<Result<Vec<Option<PathBuf>>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<PathBuf>>()
+        .into_iter()
+        .collect::<HashSet<PathBuf>>()
+        .into_iter()
+        .map(|p| {
+            let meta_path = p.join(Path::new("image.yaml"));
+            let meta_file = match File::open(meta_path) {
+                Ok(f) => f,
+                Err(e) if e.kind() == ErrorKind::NotFound => return Ok(None),
+                Err(e) => return Err(e).context("failed to open file"),
+            };
+            let config: ImageConfig = serde_yaml::from_reader(meta_file)?;
+            let spec = BuildSpec {
+                image: config.image,
+                tag:   config.tag,
+                path:  p,
+            };
+            Ok(Some(spec))
+        })
+        .collect::<Result<Vec<Option<BuildSpec>>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<BuildSpec>>();
+
+    let out = std::io::stdout();
+    serde_json::to_writer(out, &specs)?;
+    Ok(())
+}
